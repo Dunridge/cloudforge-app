0c27743a4dbf1f8aa393eb3c8f76f42ff454178f
Dunridge <Polinka7max@gmail.com>
2024-05-19 17:47:22 -0700

    post-emails. add email post

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,7 +5,8 @@ export default function Dashboard() {
     const [emails, setEmails] = useState<IEmail[]>([]);
 
     useEffect(() => {
-        fetchEmails();
+        // fetchEmails(); // (~)
+        tmpPopulateDBWithEmails();
     }, []);
 
     useEffect(() => {
@@ -14,6 +15,37 @@ export default function Dashboard() {
             debugger;
         }
     }, [emails]);
+
+    const tmpPopulateDBWithEmails = async () => {
+        const content = "Dear Jack,\nI hope this email finds you well. My name is John Doe, and I am reaching out to request a quotation for Product A and Product B. Product A comprises 100 units, while Product B consists of 50 units with custom dimensions required. The due date for this order is June 1, 2024, and the shipping address is 123 Main Street, Anytown, USA. Could you please provide a quote for the requested products, including any applicable discounts, taxes, and shipping costs? Based on previous quotes, we anticipate the following pricing: Product A at 100 units x $10/unit totaling $1000, and Product B at 50 units x $15/unit totaling $750 due to custom dimensions. The total cost is expected to be $1750. Please confirm if you can meet the specified due date and provide any additional information or documentation required to proceed with this order. Thank you for your time and assistance. I look forward to your prompt response.\n Best,\nJohn Doe\njohn.doe@example.com"
+        const email = "jack@google.com";
+        const subject = "Quotation request";
+
+        postEmail(email, subject, content);
+    }
+
+    const postEmail = async (email: string, subject: string, content: string) => { 
+        const port = 3000;
+        const postUrl = `http://localhost:${port}/api/createRFQFromEmail`;
+
+        const requestBody = { email, subject, content };
+        const strigifiedBody = JSON.stringify(requestBody);
+
+        try {
+            const response = await fetch(postUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: strigifiedBody
+            });
+            const data = await response.json();
+            debugger;
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     const fetchEmails = async () => {
         const port = 3000;
