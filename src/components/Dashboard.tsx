2e58d10ad8f3eb547b0b0c812046c562eca19763
Dunridge <Polinka7max@gmail.com>
2024-05-20 15:39:34 -0700

    emails-input. add emails fetch

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,19 +3,18 @@ import { IEmail } from "../utils/interfaces/IEmail";
 import TestFetchButton from "./TestFetchButton";
 
 export default function Dashboard() {
-    const [emails, setEmails] = useState<IEmail[]>([]);
 
     useEffect(() => {
         // fetchEmails(); // (~)
         // tmpPopulateDBWithEmails(); (~)
     }, []);
 
-    useEffect(() => {
-        if (emails.length > 0) {
-            console.log('emails', emails);
-            debugger;
-        }
-    }, [emails]);
+    // useEffect(() => {
+    //     if (emails.length > 0) {
+    //         console.log('emails', emails);
+    //         debugger;
+    //     }
+    // }, [emails]);
 
     const tmpPopulateDBWithEmails = async () => {
         const content = "Dear Jack,\nI hope this email finds you well. My name is John Doe, and I am reaching out to request a quotation for Product A and Product B. Product A comprises 100 units, while Product B consists of 50 units with custom dimensions required. The due date for this order is June 1, 2024, and the shipping address is 123 Main Street, Anytown, USA. Could you please provide a quote for the requested products, including any applicable discounts, taxes, and shipping costs? Based on previous quotes, we anticipate the following pricing: Product A at 100 units x $10/unit totaling $1000, and Product B at 50 units x $15/unit totaling $750 due to custom dimensions. The total cost is expected to be $1750. Please confirm if you can meet the specified due date and provide any additional information or documentation required to proceed with this order. Thank you for your time and assistance. I look forward to your prompt response.\n Best,\nJohn Doe\njohn.doe@example.com"
@@ -87,20 +86,20 @@ export default function Dashboard() {
         }
     }
 
-    const fetchEmails = async () => {
-        const port = 3000;
-        // TODO: put this url into a config 
-        const fetchUrl = `http://localhost:${port}/api/getCustomerEmails`;
+    // const fetchEmails = async () => {
+    //     const port = 3000;
+    //     // TODO: put this url into a config 
+    //     const fetchUrl = `http://localhost:${port}/api/getCustomerEmails`;
 
-        try {
-            const response = await fetch(fetchUrl);
-            const data = await response.json();
-            console.log(data);
-            setEmails(data);
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    //     try {
+    //         const response = await fetch(fetchUrl);
+    //         const data = await response.json();
+    //         console.log(data);
+    //         setEmails(data);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }
 
     const createRFQFromEmail = async (emailAdress: string, subject: string, content: string, isRFQable: boolean) => {
         const port = 3000;
@@ -131,7 +130,7 @@ export default function Dashboard() {
         <>
             <div>Dashboard</div>
             <div className="flex gap-[20px]">
-                <TestFetchButton title="Fetch Emails" doAction={fetchEmails} />
+                {/* <TestFetchButton title="Fetch Emails" doAction={fetchEmails} /> */}
                 <TestFetchButton title="Post Emails" doAction={tmpPopulateDBWithEmails} />
                 <TestFetchButton title="Check Whether RFQ" doAction={tmpCheckWhetherEmailIsRFQ} />
                 <TestFetchButton title="Create RFQ From Email" doAction={tmpCreateRFQFromEmail} />
--- src/components/EmailInput.tsx
+++ src/components/EmailInput.tsx
@@ -1,6 +1,52 @@
+import { useEffect, useState } from "react";
+import { IEmail } from "../utils/interfaces/IEmail";
+import TestFetchButton from "./TestFetchButton";
 
 export default function EmailInput() {
+    const [emails, setEmails] = useState<IEmail[]>([]);
+
+    useEffect(() => {
+        fetchEmails();
+    }, [])
+
+    useEffect(() => {
+        if (emails.length > 0) {
+            console.log('emails', emails);
+            debugger;
+        }
+    }, [emails]);
+
+    const fetchEmails = async () => {
+        const port = 3000;
+        // TODO: put this url into a config 
+        const fetchUrl = `http://localhost:${port}/api/getCustomerEmails`;
+
+        try {
+            const response = await fetch(fetchUrl);
+            const data = await response.json();
+            console.log(data);
+            debugger;
+            setEmails(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
-        <div>EmailInput</div>
+        <div>
+            <div>EmailInput</div>
+            {/* <TestFetchButton title="Fetch Emails" doAction={fetchEmails} /> */}
+            {/* TODO: add sub routes to the UIs for the output of emails and then for inputting the email text */}
+
+            <div>
+                {/* { JSON.stringify(emails) } */}
+                { Array.isArray(emails) && emails.map((emailObj) => {
+                    debugger;
+                    return (
+                        <div>Test { `${emailObj?.email}` }</div>
+                    );
+                })}
+            </div>
+        </div>
     )
 }
