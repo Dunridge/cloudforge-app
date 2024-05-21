0be9e3e0aa82f42f5872cf9430ae318697766f0b
Dunridge <Polinka7max@gmail.com>
2024-05-21 11:28:11 -0700

    add-rfq. add endpoint to check whether order can be filled

--- src/components/RFQCard.tsx
+++ src/components/RFQCard.tsx
@@ -37,6 +37,42 @@ export default function RFQCard({ emailAdress, emailText, subject, isInSentQuote
 
     }
 
+    const checkWhetherRFQCanBeFilled = async (emailTextData: string) => {
+        // emailText
+        // checkWhetherRFQCanBeFilled
+        const port = 3000;
+        const postUrl = `http://localhost:${port}/api/checkWhetherRFQCanBeFilled`;
+        const requestBody = { emailText: emailTextData }; // this is sent to the endpoint
+        const strigifiedBody = JSON.stringify(requestBody);
+        debugger;
+
+        try {
+            const response = await fetch(postUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: strigifiedBody
+            });
+            const data = await response.json();
+            if (response.ok && data.canBeFilled) {
+                toast.info('RFQ can be filled');
+                debugger;
+            } else {
+                toast.error('RFQ cannot be filled');
+            }
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+            debugger;
+        }
+    }
+
+    const handleWhetherRFQCanBeFilled = () => {
+        debugger;
+        checkWhetherRFQCanBeFilled(emailText);
+    }
+
     return (
         <div className="flex flex-col w-[420px] bg-white px-4 py-3 border border-gray-200 rounded rounded-md">
             <div className="flex gap-[8px]">
@@ -53,6 +89,7 @@ export default function RFQCard({ emailAdress, emailText, subject, isInSentQuote
                 !isInSentQuotes && (
                     <div className="flex flex-col mt-[10px] gap-[8px]">
                         <button onClick={handleSendRFQEmail} className={`px-[12px] py-[2px] bg-black text-white`}>Send RFQ</button>
+                        <button onClick={handleWhetherRFQCanBeFilled} className={`px-[12px] py-[2px] bg-black text-white`}>Check whether RFQ can be filled</button>
                     </div>
                 )
             }
