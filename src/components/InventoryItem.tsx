79ea63817a1ac181b06fe92e367c3193b10604e1
Dunridge <Polinka7max@gmail.com>
2024-05-21 13:20:56 -0700

    add-rfq. add mapping

--- src/components/InventoryItem.tsx
+++ src/components/InventoryItem.tsx
@@ -1,11 +1,13 @@
 import { IInventoryItem } from "../utils/interfaces/IInventoryItem";
 
-export default function InvetoryItem({ name, quantity }: IInventoryItem) {
+export default function InvetoryItem({ name, quantity, price }: IInventoryItem) {
 
     return (
         <div className="w-full flex justify-between bg-white px-[20px] py-[12px]">
-            <div>{name}</div>
-            <div>{quantity}</div>
+            <div>Name: {name}</div>
+            <div>Quantity: {quantity}</div>
+            <div>Price (per unit): { price }</div>
+            {/* 3000 - 1000 */}
         </div>    
     );
 }
